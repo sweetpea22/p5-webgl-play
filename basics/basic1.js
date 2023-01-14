68949c39a8b89a344a0b608c4daf77671cb9dc7e
sweetpea22 <cindychau24@me.com>
2023-01-14 10:37:53 -0500

    Add

--- basics/basic1.js
+++ basics/basic1.js
@@ -2,22 +2,21 @@
 let y = 100;
 let num = 2000;
 let a = 40;
-let r1, r2, r3, r4;
 
-class MRect {
-  constructor(iw, ixp, ih, iyp) {
-    this.w = iw; // individual width;
-    this.xpos = ixp; // rect x position;
-    this.h = ih; // rect height
-    this.ypos = iyp; // rect yposition
-  }
-
-  display() {
-    for (let i = 0; i < num; i++) {
-      rect(this.xpos, this.ypos, this.w, this.height * this.h);
-    }
-  }
-}
+const bubble = {
+  x: 100,
+  y: 200,
+  move: function () {
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
+  },
+  display: function () {
+    stroke(255);
+    strokeWeight(5);
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  },
+};
 function slideLeft() {
   a = a - 35;
 
@@ -39,23 +38,18 @@ console.log(getRandomYPosition());
 
 function setup() {
   createCanvas(1000, 800);
-  fill(255, 139, 19);
-  noStroke();
-
-  r1 = new MRect(20, 20, 40, 30);
-
   frameRate(60);
 }
 
 function draw() {
   background(0);
-  a = a - 20;
+  bubble.move();
+  bubble.display();
+  // a = a - 20;
 
-  if (a < 0) {
-    a = width;
-  }
-  r1.display();
-  // rect(a, 20, a / 5, 20);
+  // if (a < 0) {
+  //   a = width;
+  // }
 }
 
 // for (let i; i < num; i++) {
